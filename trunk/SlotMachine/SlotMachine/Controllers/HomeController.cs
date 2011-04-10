71477c2e2ebf340ab331e51dfaaf21bae46fec12
syuraj <syuraj@b271608f-7367-0db8-9110-f29ecae5b2b6>
2011-04-10 14:51:05 +0000

    First time check in. App Does not work properly. Checking in for historical purpose only.

---
+++ trunk/SlotMachine/SlotMachine/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SlotMachine.Models;
+using Facebook;
+using Facebook.Web;
+using Facebook.Web.Mvc;
+using System.Web.Routing;
+
+namespace SlotMachine.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IFormsAuthenticationService FormsService { get; set; }        
+
+        protected override void Initialize(RequestContext requestContext)
+        {
+            if (FormsService == null) { FormsService = new FormsAuthenticationService(); }            
+            base.Initialize(requestContext);
+        }
+
+        [CanvasAuthorize(Permissions = "user_about_me")]
+        public ActionResult Index()
+        {            
+            LogInUser();            
+            return View();
+        }
+
+        public ActionResult Play()
+        {
+            return View();
+        }
+
+        private void LogInUser()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                var fb = new FacebookWebClient();
+                dynamic result = fb.Get("me");
+                FormsService.SignIn(result.id, true);
+            }
+        }        
+    }
+}
---
+++ trunk/SlotMachine/SlotMachine/Models/InMemoryUserStore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SlotMachine.Models
+{
+    public class InMemoryUserStore
+    {
+
+        private static System.Collections.Concurrent.ConcurrentBag<AppUser> users = new System.Collections.Concurrent.ConcurrentBag<AppUser>();
+
+        public static void Add(AppUser user)
+        {
+            if (users.SingleOrDefault(u => u.FacebookId == user.FacebookId) != null)
+            {
+                throw new InvalidOperationException("User already exists.");
+            }
+
+            users.Add(user);
+        }
+
+        public static AppUser Get(long facebookId)
+        {
+            return users.SingleOrDefault(u => u.FacebookId == facebookId);
+        }
+
+    }
+}
